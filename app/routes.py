e64ea1b641be457efe27bb8e0956fc88a76758dc
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-03 22:16:19 +0100

    WIP: the user profile page

--- app/routes.py
+++ app/routes.py
@@ -60,3 +60,13 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
