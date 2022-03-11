17b79d4cd9fe9e7d764838513ae5c4c293ccb8a7
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-11 09:23:07 +0100

    Feat: added follow and unfollow functionalities

--- app/models.py
+++ app/models.py
@@ -40,6 +40,19 @@ class User(UserMixin, db.Model):
             digest, size
         )
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(280))
