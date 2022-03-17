83ebb271567074e22c79412c558bd1be00a74505
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-17 07:12:52 +0100

    Feat: pagination for posts

--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,21 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home', posts=posts, form=form)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index.html', page=posts.prev_num) if posts.has_prev else None
+    # Changing posts to posts.items since a pagination object is being returned now
+    return render_template(
+        'index.html', 
+        title='Home', 
+        posts=posts.items, 
+        form=form,
+        next_url=next_url,
+        prev_url=prev_url
+    )
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -126,10 +139,21 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     # Reusing the index template here and ignoring the form argument since we don't want a form
     # on the page
-    return render_template('index.html', title='Explore', posts=posts)
+    return render_template(
+        'index.html', 
+        title='Explore', 
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url
+    )
 
 @app.before_request
 def before_request():
