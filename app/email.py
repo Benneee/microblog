5ee0ca46d79df60ba3a2ed0e3757d13c3220dcbc
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-17 14:22:36 +0100

    Feat: making the email sending action asynchronous

--- app/email.py
+++ app/email.py
@@ -1,13 +1,17 @@
 from flask_mail import Message
 from flask import render_template
+from threading import Thread
 from app import app, mail
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
