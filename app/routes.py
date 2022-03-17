5e0cac4d376366f949856c04bbf971d5a7127c75
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-17 07:28:27 +0100

    Feat: pagination for posts in user profile page

--- app/routes.py
+++ app/routes.py
@@ -76,12 +76,21 @@ def logout():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template(
+        'user.html', 
+        user=user, 
+        posts=posts.items, 
+        next_url=next_url, 
+        prev_url=prev_url, 
+        form=form
+    )
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
