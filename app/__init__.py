dd065f99ce6ab39c6bff28e96e9297bbcb7195a1
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-19 23:12:06 +0100

    WIP: internationalisation and localization

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from logging.handlers import RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 import os
 
 app = Flask(__name__)
@@ -19,6 +21,11 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if not os.path.exists('logs'):
