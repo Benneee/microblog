235a4be15b0f41fafe1a994627b0045d0ada6f88
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-03-30 22:08:37 +0100

    WIP: Notifications - private messages

--- app/main/forms.py
+++ app/main/forms.py
@@ -40,3 +40,11 @@ class SearchForm(FlaskForm):
         if 'meta' not in kwargs:
             kwargs['meta'] = {'csrf': False}
         super(SearchForm, self).__init__(*args, **kwargs)
+
+
+class MessageForm(FlaskForm):
+    message = TextAreaField(_l('Message'), validators=[
+        DataRequired(),
+        Length(min=0, max=140)
+    ])
+    submit = SubmitField(_l('Submit'))
