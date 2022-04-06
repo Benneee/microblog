1185742cce3a21d6ee15610fc372d595a2c5a26d
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-04-06 22:44:54 +0100

    Feat: added export functionality

--- app/tasks.py
+++ app/tasks.py
@@ -2,7 +2,11 @@ from rq import get_current_job
 import time
 from rq import get_current_job
 from app import create_app, db
-from app.models import Task
+from app.models import User, Post, Task
+import sys
+import json
+from flask import render_template
+from app.email import send_email
 
 
 app = create_app()
@@ -37,3 +41,43 @@ def example(seconds):
     job.meta['progress'] = 100
     job.save_meta()
     print('Task completed')
+
+
+def export_posts(user_id):
+    try:
+        user = User.query.get(user_id)
+        _set_task_progress(0)
+        data = []
+        i = 0
+        total_posts = user.posts.count()
+        for post in user.posts.order_by(Post.timestamp.asc()):
+            data.append({ 
+                'body': post.body,
+                'timestamp': post.timestamp.isoformat() + 'Z'
+            })
+            time.sleep(5)
+            i += 1
+            _set_task_progress(100 * i)
+        
+        send_email(
+            '[Microblog] Your blog posts',
+            sender=app.config['ADMINS'][0], 
+            recipients=[user.email],
+            text_body=render_template('email/export_posts.txt', user=user),
+            html_body=render_template('email/export_posts.html', user=user),
+            attachments=[
+                (
+                    'posts.json',
+                    'application/json',
+                    json.dumps(
+                        {'posts': data},
+                        indent=4
+                    )
+                )
+            ],
+            sync=True
+        )
+    except:
+        app.logger.error('Unhandled exception', exc_info=sys.exc_info())
+    finally:
+        _set_task_progress(100)
